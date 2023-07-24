2653c079ddb66bc79646c21b344d3d11ba28dd94
grap3day <124905192+grap3day@users.noreply.github.com>
2023-07-25 01:39:05 +0300

    Update UserService.java

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public interface UserService {
     void saveUser(User user);
-//    User updateUser(User user);
     void deleteUserFromTable(Long id);
     List<User> getAllUsers();
     User getUserById(Long id);
